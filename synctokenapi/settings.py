0061b15c1982db02b079f83c14a0697ab95abfcb
Davi Souza de Oliveira <vlogsplayer10@gmail.com>
2024-07-07 11:42:14 -0300

    db configured correctly (i think)

--- synctokenapi/settings.py
+++ synctokenapi/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'synctokenapi.wsgi.application'
 
 DATABASES = {
 
-    'default': dj_database_url.config(default=os.environ.get('DATABASE'))
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
