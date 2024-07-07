b1a174bb7dc3ea1ef40464a08ad5394b1c19e07f
Davi Souza de Oliveira <vlogsplayer10@gmail.com>
2024-07-07 11:51:28 -0300

    debug = false

--- synctokenapi/settings.py
+++ synctokenapi/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRETKEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True  # Avoid it
+DEBUG = False  # Avoid it
 
 ALLOWED_HOSTS = ["*"]
 
